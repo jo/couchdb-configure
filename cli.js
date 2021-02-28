17544abfb5c6ec2852947cc11d76538a46acf0d9
Johannes Jörg Schmidt <schmidt@pm.me>
2021-02-28 14:09:07 +0100

    feat: upgrade dependencies
    BREAKING CHANGE:
    This could break your code bc of the nano upgrade to v9 (via nano-option). Please refer to [nano documentation](https://github.com/apache/couchdb-nano/).
    
    Updated packages:
    * async              ^2.6.0  →    ^3.2.0
    * couchdb-compile   ^1.10.0  →   ^1.11.0
    * nano-option        ^1.3.0  →    ^2.0.0
    * nano               ^6.4.3  →    ^9.0.3
    * standard          ^11.0.0  →   ^16.0.3
    * tap              ^14.10.7  →  ^14.11.0

--- cli.js
+++ cli.js
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
 
-var configure = require('./')
+const configure = require('./')
 
-var args = process.argv.slice(2)
+const args = process.argv.slice(2)
 if (!args.length) {
   console.log('Usage: \ncouchdb-configure URL [SOURCE]')
   process.exit()
 }
 
-var url = args[0]
-var source = args[1] || process.cwd()
+const url = args[0]
+const source = args[1] || process.cwd()
 
 configure(url, source, function (error, response) {
   if (error) return console.error(error)
